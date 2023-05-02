8c1377c0c60920528950e4b67779a797421af0c2
Edward Kuan <edward880814@gmail.com>
2023-05-02 16:12:44 +0800

    feat: project initialization

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const router = require("./routes");
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+app.use(router);
+
+app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
