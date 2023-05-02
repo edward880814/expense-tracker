107a37c4d811fc35d22075109cfd190c8ea6d02c
Edward Kuan <edward880814@gmail.com>
2023-05-02 18:32:03 +0800

    feat: setup mongooose.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+
 const router = require("./routes");
 const PORT = process.env.PORT || 3000
 
@@ -10,7 +8,6 @@ const app = express();
 
 app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
-app.use(express.static("public"));
 
 app.use(router);
 
