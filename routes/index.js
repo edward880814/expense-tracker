ec1ea6b8f594b9a3fa005139d128e304afa916e4
Edward Kuan <edward880814@gmail.com>
2023-05-03 14:22:35 +0800

    feat: install connect-flash and use it

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const records = require("./modules/records");
 const users = require("./modules/users");
 const { authenticator } = require("../middleware/auth.js");
 
+
+
 //要注意載入的順序
-router.use("/records", authenticator, records);
 router.use("/users", users);
+router.use("/records", authenticator, records);
 router.use("/", authenticator, home);
 
 module.exports = router;
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const Record = require("../../models/Record");
 const passport = require("passport");
 
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+
+//Login
 router.get("/login", (req, res) => {
   res.render("login");
 });
@@ -13,4 +17,46 @@ router.post("/login",
     failureRedirect: "/users/login",
   })
 );
+
+//Logout
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    req.flash("success_msg", "Successfully Logout");
+    res.redirect("/users/login");
+  });
+});
+
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+
+//Register
+router.post("/register", (req, res) => {
+  const { name, password, confirmPassword } = req.body;
+  User.findOne({ name })
+    .then((user) => {
+      if (user) {
+        req.flash("error_msg", "Account already exist！");
+        return res.redirect("register");
+      }
+      if (password !== confirmPassword) {
+        req.flash("error_msg", "password is not equal to confirmpassword!");
+        return res.redirect("register");
+      }
+
+      bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(password, salt))
+        .then((hash) => {
+          User.create({ name, password: hash }).then(() => {
+            req.flash("success_msg", "Successfully register!");
+            res.redirect("/");
+          });
+        });
+    })
+    .catch((err) => console.log(err));
+});
 module.exports = router;
