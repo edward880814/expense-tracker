1f6e2cab400e49ad54950093d66cbb1636c1456e
Edward Kuan <edward880814@gmail.com>
2023-05-03 14:11:40 +0800

    feat: add login page

---
+++ routes/modules/users.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const Record = require("../../models/Record");
+const passport = require("passport");
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post("/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
+  })
+);
+module.exports = router;
