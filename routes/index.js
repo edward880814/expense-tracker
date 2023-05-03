1e0a3f07adda98e8add2e245c8f238908a35cdf5
Edward Kuan <edward880814@gmail.com>
2023-05-03 14:02:33 +0800

    feat: add authentication in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const home = require("./modules/home");
 const records = require("./modules/records");
-router.use("/records", records);
-router.use("/", home);
+const users = require("./modules/users");
+const { authenticator } = require("../middleware/auth.js");
+
+//要注意載入的順序
+router.use("/records", authenticator, records);
+router.use("/users", users);
+router.use("/", authenticator, home);
+
 module.exports = router;
