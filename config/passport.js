49a6f1ce709085fc95925e8d494290219c9e515f
Edward Kuan <edward880814@gmail.com>
2023-05-03 13:56:06 +0800

    feat: set passport.js

---
+++ config/passport.js
@@ -0,0 +1,40 @@
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+module.exports = (app) => {
+  
+  //initialization
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+
+  //set local authentication mechanism
+  passport.use(
+    new localStrategy({ usernameField: "name" }, (name, password, done) => {
+      User.findOne({ name })
+        .then((user) => {
+          if (!user) return done(null, false, { message: "user doesn't exit" });
+          bcrypt.compare(password, user.password).then((isMatch) => {
+            if (!isMatch)
+              return done(null, false, { message: "password is incorrect" });
+          });
+          return done(null, user);
+        })
+        .catch((err) => done(err, false));
+    })
+  );
+
+
+  
+
+  passport.serializeUser((user, done) => done(null, user.id));
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .lean()
+      .then((user) => done(null, user))
+      .catch((err) => done(err, null));
+  });
+};
