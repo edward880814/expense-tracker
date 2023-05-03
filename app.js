f745c383dd19ced36b86e6fc8e497b83dc030627
Edward Kuan <edward880814@gmail.com>
2023-05-03 14:58:56 +0800

    feat: check if any error

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ require("./config/mongoose");
 const methodOverride = require("method-override");
 const session = require("express-session")
 const usePassport = require("./config/passport");
+const flash = require("connect-flash");
+
 
 const PORT = process.env.PORT || 3000
 
@@ -43,10 +45,13 @@ app.use(
 
 
 usePassport(app);
+app.use(flash());
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
   next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const home = require("./modules/home");
 const records = require("./modules/records");
+
 const users = require("./modules/users");
 const { authenticator } = require("../middleware/auth.js");
 
