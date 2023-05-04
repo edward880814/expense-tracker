4eea89ac9afef8fb3645ddb0ccb1762cc7fe2b21
Edward Kuan <edward880814@gmail.com>
2023-05-04 11:16:18 +0800

    feat: fix the problem about home.js again

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,32 +3,33 @@ const router = express.Router();
 const Record = require("../../models/Record");
 const Category = require("../../models/Category");
 
-router.get("/", (req, res) => {
-  const userId = req.user._id;
-  Record.find({ userId })
-    .lean()
-    .sort({ date: "desc" })
-    .then((records) => {
-      let totalAmount = 0;
-      const promises = records.map((record) => {
-        return Category.findById(record.categoryId)
-          .lean()
-          .then((category) => {
-            const categoryIcon = category.icon;
 
-            const formattedRecord = {
-              ...record,
-              categoryIcon,
-              date: new Date(record.date).toLocaleString().substring(0, 9),
-            };
-            totalAmount += record.amount;
-            return formattedRecord;
-          });
-      });
-      Promise.all(promises).then((formattedRecords) => {
-        res.render("index", { records: formattedRecords, totalAmount });
-      });
-    });
+router.get("/", (req, res) => {
+  async function getFormattedRecords() {
+    const userId = req.user._id;
+    const records = await Record.find({ userId }).lean().sort({ date: "desc" });
+    let totalAmount = 0;
+    const formattedRecords = await records.reduce(async (accPromise, record) => {
+      const acc = await accPromise;
+      const category = await Category.findById(record.categoryId).lean();
+      const categoryIcon = category.icon;
+      const formattedRecord = formatRecord(record, categoryIcon);
+      totalAmount += record.amount;
+      return [...acc, formattedRecord];
+    }, Promise.resolve([]));
+
+    return { formattedRecords, totalAmount };
+  }
+
+  function formatRecord(record, categoryIcon) {
+    const formattedDate = new Date(record.date).toLocaleString().substring(0, 9);
+    const formattedRecord = Object.assign({}, record, { categoryIcon, date: formattedDate });
+    return formattedRecord;
+  }
+
+  getFormattedRecords().then(({ formattedRecords, totalAmount }) => {
+    res.render("index", { records: formattedRecords, totalAmount });
+  });
 });
 
 module.exports = router;
